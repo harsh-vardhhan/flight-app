1b2211317c9942128f804eb109a97bd44156541e
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2025-05-16 12:37:53 +0530

    Update navigation.tsx

--- app/navigation.tsx
+++ app/navigation.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import FlightListScreen from './screens/FlightListScreen';
-import {useTheme} from './../hooks/useTheme';
+import React from "react";
+import { SafeAreaView, StatusBar } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import FlightListScreen from "./screens/FlightListScreen";
+import { useTheme } from "./../hooks/useTheme";
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
-    const {isDarkMode} = useTheme();
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: isDarkMode ? '#1C2526' : 'white' }}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={isDarkMode ? '#1C2526' : 'white'}
-            />
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Flights" component={FlightListScreen} />
-            </Stack.Navigator>
-        </SafeAreaView>
-    )
+  const { isDarkMode } = useTheme();
+  return (
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: isDarkMode ? "#1C2526" : "white" }}
+    >
+      <StatusBar
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
+        backgroundColor={isDarkMode ? "#1C2526" : "white"}
+      />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Flights" component={FlightListScreen} />
+      </Stack.Navigator>
+    </SafeAreaView>
+  );
 };
 
 export default AppNavigator;
